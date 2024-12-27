17d9874d888c0069394109b12b17af7739f6a347
Cat Miller <catmiller.cm@gmail.com>
2024-12-26 23:42:04 -0600

    use react intersection observer to animate only when on screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,44 @@
+'use client';
+
+import { useInView } from 'react-intersection-observer';
 import styles from './entry.module.css';
 
 const Page = () => {
+  const animals = [
+    'manatee',
+    'turtle',
+    'barracuda',
+    'starfish',
+    'seahorse',
+    'stingray',
+    'mantaRay',
+    'eagleRay',
+    'reefShark',
+    'otherShark',
+    'eel',
+    'fish1',
+    'fish2',
+    'fish3',
+    'fish4'
+  ];
+
   return (
     <main className={styles.main}>
-      <p className={styles.manatee}>MANATEE</p>
-      <p className={styles.turtle}>TURTLE</p>
-      <p className={styles.barracuda}>BARACUDA</p>
-      <p className={styles.starfish}>STARFISH</p>
-      <p className={styles.seahorse}>SEAHORSE</p>
-      <p className={styles.stingray}>STINGRAY</p>
-      <p className={styles.mantaRay}>MANTARAY</p>
-      <p className={styles.eagleRay}>EAGLERAY</p>
-      <p className={styles.reefShark}>REEFSHARK</p>
-      <p className={styles.otherShark}>OTHERSHARK</p>
-      <p className={styles.eel}>EEL</p>
-      <p className={styles.fish1}>FISH1</p>
-      <p className={styles.fish2}>FISH2</p>
-      <p className={styles.fish3}>FISH3</p>
-      <p className={styles.fish4}>FISH4</p>
+      {animals.map((animal) => {
+        const [ref, inView] = useInView({ threshold: 0.25, triggerOnce: true });
+
+        return (
+          <p
+            key={animal}
+            ref={ref}
+            className={`${styles[animal]} ${inView ? styles.animate : ''}`}
+          >
+            {animal}
+          </p>
+        );
+      })}
     </main>
   );
-}
+};
 
 export default Page;
