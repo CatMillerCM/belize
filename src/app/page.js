9887a346b7ad03fb795638eff31933c32308d30e
Cat Miller <catmiller.cm@gmail.com>
2024-12-26 21:56:06 -0600

    add basic p tag animals and initial css positioning

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,21 @@ import styles from './entry.module.css';
 const Page = () => {
   return (
     <main className={styles.main}>
+      <p className={styles.manatee}>MANATEE</p>
+      <p className={styles.turtle}>TURTLE</p>
+      <p className={styles.barracuda}>BARACUDA</p>
+      <p className={styles.starfish}>STARFISH</p>
+      <p className={styles.seahorse}>SEAHORSE</p>
+      <p className={styles.stingray}>STINGRAY</p>
+      <p className={styles.mantaRay}>MANTARAY</p>
+      <p className={styles.eagleRay}>EAGLERAY</p>
+      <p className={styles.reefShark}>REEFSHARK</p>
+      <p className={styles.otherShark}>OTHERSHARK</p>
+      <p className={styles.eel}>EEL</p>
+      <p className={styles.fish1}>FISH1</p>
+      <p className={styles.fish2}>FISH2</p>
+      <p className={styles.fish3}>FISH3</p>
+      <p className={styles.fish4}>FISH4</p>
     </main>
   );
 }
