991363fdde92d394a17683dccbeac7f0cf90e18c
Cat Miller <catmiller.cm@gmail.com>
2024-12-27 23:40:43 -0600

    insert assets

---
+++ src/assets/index.js
@@ -0,0 +1,32 @@
+import angelfish from './angelfish.png';
+import barracuda from './barracuda.png';
+import blueTang from './blue-tang.png';
+import eagleRay from './eagle-ray.png';
+import eel from './eel.png';
+import manatee from './manatee.png';
+import mantaRay from './manta-ray.png';
+import nurseShark from './nurse-shark.png';
+import parrotfish from './parrotfish.png';
+import seahorse from './seahorse.png';
+import starfish from './starfish.png';
+import stingray from './stingray.png';
+import turtle from './turtle.png';
+
+const animalImages = {
+  angelfish,
+  barracuda,
+  blueTang,
+  eagleRay,
+  eel,
+  manatee,
+  mantaRay,
+  nurseShark,
+  parrotfish,
+  seahorse,
+  starfish,
+  stingray,
+  turtle  
+};
+
+
+export { animalImages };
